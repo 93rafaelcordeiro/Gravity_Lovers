27437a4a4dc7751f8a90a9a67489f4eb54d7040f
codecadet <andrecfreitas58@gmail.com>
2021-10-09 15:29:21 +0000

    Add grid

---
+++ src/org/academiadecodigo/bootcamp65/grid/GridColor.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.bootcamp65.grid;
+
+/**
+ * The available grid colors
+ */
+public enum GridColor {
+
+    RED,
+    GREEN,
+    BLUE,
+    MAGENTA,
+    NOCOLOR,
+    YELLOW
+
+}
