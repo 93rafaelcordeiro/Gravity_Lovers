d54b5cc5d34e95be94d173b8242bf072f7a8e0c0
andresatierf <andrecfreitas58gmail.com>
2021-10-10 21:08:43 +0000

    Add basic game testing

--- src/org/academiadecodigo/bootcamp65/Game.java
+++ src/org/academiadecodigo/bootcamp65/Game.java
@@ -5,30 +5,147 @@ import org.academiadecodigo.bootcamp65.grid.GridFactory;
 import org.academiadecodigo.bootcamp65.grid.GridType;
 import org.academiadecodigo.bootcamp65.handler.GameKeyboardHandler;
 import org.academiadecodigo.bootcamp65.levels.Level;
-import org.academiadecodigo.bootcamp65.levels.LevelFactory;
+import org.academiadecodigo.bootcamp65.levels.LevelType;
+import org.academiadecodigo.bootcamp65.objects.Barrier;
+import org.academiadecodigo.bootcamp65.objects.Player;
+import org.academiadecodigo.bootcamp65.physics.Vector;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Text;
 
 public class Game {
 
+    public static final int GRID_WIDTH = 800;
+    public static final int GRID_HEIGHT = 600;
     private Grid grid;
     private Level level;
+    private Player player;
+    private double gravityPull;
+    private Vector gravity;
+    private Vector wind;
+    private String directionLabel;
+    private Text direction;
 
     public Game() {
-        this.grid = GridFactory.makeGrid(GridType.SIMPLE_GFX,1200, 900);
+        this.grid = GridFactory.makeGrid(GridType.SIMPLE_GFX, GRID_WIDTH, GRID_HEIGHT);
     }
 
     public void init() {
+        // Handler
+        new GameKeyboardHandler(this);
+
         grid.init();
-        level = LevelFactory.createLevel(1);
 
-        new GameKeyboardHandler();
+        //level = LevelFactory.createLevel(1);
+        this.gravityPull = .5;
+        this.gravity = new Vector(0, this.gravityPull);
+
+        this.level = LevelType.getLevel_1();
+
+        this.player = new Player(50, 550, 30, 30);
+
+        this.directionLabel = "↓";
+        this.direction = new Text(this.grid.getCols() - 22.5, 32.5, this.directionLabel);
+        this.direction.grow(10, 10);
+        this.direction.setColor(Color.WHITE);
+        this.direction.draw();
     }
 
-    public void start() {
+    public void start() throws InterruptedException {
+
+        while (true) {
+            System.out.println("Pos: " + this.player.getPosition().getX() + ":" + this.player.getPosition().getY());
+            drawLevel();
+            drawPlayer();
+            applyAcceleration();
+            update();
 
 
+            Thread.sleep(10);
+        }
     }
+
+    //region Getters and Setters
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public double getGravityPull() {
+        return gravityPull;
+    }
+
+    public void setGravityPull(double gravityPull) {
+        this.gravityPull = gravityPull;
+    }
+
+    public Vector getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(Vector gravity) {
+        this.gravity = gravity;
+    }
+
+    public Vector getWind() {
+        return wind;
+    }
+
+    public void setWind(Vector wind) {
+        this.wind = wind;
+    }
+
+    public String getDirectionLabel() {
+        return directionLabel;
+    }
+
+    public void setDirectionLabel(String directionLabel) {
+        this.directionLabel = directionLabel;
+    }
+
+    public Text getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Text direction) {
+        this.direction = direction;
+    }
+
+    //endregion
 
     public void drawLevel() {
+        this.direction.delete();
+        this.direction.draw();
         this.level.show();
+    }
+
+    public void drawPlayer() {
+        this.player.show();
+    }
+
+    public void applyAcceleration() {
+        this.player.addAcceleration(this.gravity);
+    }
+
+    public void update() {
+        this.player.update(level);
     }
 }
