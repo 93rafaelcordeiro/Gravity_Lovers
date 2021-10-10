fd8853bcba59e1c8e15e2f72a39ae48c7ffcceb3
andresatierf <andrecfreitas58gmail.com>
2021-10-10 21:10:50 +0000

    Add barrier type and trim old collision detector

--- src/org/academiadecodigo/bootcamp65/CollisionDetector.java
+++ src/org/academiadecodigo/bootcamp65/CollisionDetector.java
@@ -1,29 +1,16 @@
 package org.academiadecodigo.bootcamp65;
 
-import org.academiadecodigo.bootcamp65.grid.position.GridPosition;
 import org.academiadecodigo.bootcamp65.objects.GameObject;
 
 public class CollisionDetector {
 
-    private Car[] cars;
+    private GameObject[] gameObjects;
 
-    public CollisionDetector(Car[] cars) {
-        this.cars = cars;
+    public CollisionDetector(GameObject[] gameObjects) {
+        this.gameObjects = gameObjects;
     }
 
-    public boolean isUnSafe(GridPosition pos) {
 
-        for (Car c : cars) {
-
-            if (c.getPos() != pos && c.getPos().equals(pos)) {
-                return true;
-            }
-
-        }
-
-        return false;
-
-    }
 
     public void check(GameObject gameObject) {
 
---
+++ src/org/academiadecodigo/bootcamp65/objects/BarrierType.java
@@ -0,0 +1,15 @@
+package org.academiadecodigo.bootcamp65.objects;
+
+public enum BarrierType {
+    ONE_BY_ONE,
+    ONE_BY_TWO,
+    ONE_BY_THREE,
+    ONE_BY_FOUR,
+
+    TWO_BY_TWO,
+    TWO_BY_THREE,
+    TWO_BY_FOUR,
+
+    THREE_BY_THREE,
+    THREE_BY_FOUR,
+}
